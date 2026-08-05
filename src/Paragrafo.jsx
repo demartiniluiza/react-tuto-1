197ee00a5aceadb371c0254fe6d16da161219283
Derick <derickv.sonda@gmail.com>
2026-08-05 11:12:47 -0300

    COMMIT

---
+++ src/Paragrafo.jsx
@@ -0,0 +1,14 @@
+import macaco from './assets/images.jpg'
+
+function Paragrafo(){
+
+    return (
+        <>
+            <p>Paragráfo explicativo</p>
+            <img src={macaco} alt="Macaco sorrindo" width="300
+            " />
+        </>
+    )
+}
+
+export default Paragrafo
